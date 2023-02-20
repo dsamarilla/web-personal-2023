79a2724fef12823b0ce888f9aedd86dd8a1e62f7
dsamarilla <denissaul87@gmail.com>
2023-02-19 22:23:14 -0300

    Hasta clase 64, eliminar cursos

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(morgan("dev"));
 const authRoutes = require("./router/auth");
 const userRoutes = require("./router/user");
 const menuRoutes = require("./router/menu");
+const courseRoutes = require("./router/course");
 
 // configure body parser
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -27,5 +28,6 @@ app.use(cors());
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
 app.use(`/api/${API_VERSION}`, menuRoutes);
+app.use(`/api/${API_VERSION}`, courseRoutes);
 
 module.exports = app;
